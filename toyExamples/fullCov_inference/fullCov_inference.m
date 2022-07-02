b8ae6af636f91f859ac351a1e364e27086ae0d16
Mr.R <afakhri@pm.me>
2022-07-01 22:47:47 -0400

    big fix, refactor, added grid/random search

--- toyExamples/fullCov_inference/fullCov_inference.m
+++ toyExamples/fullCov_inference/fullCov_inference.m
@@ -10,7 +10,7 @@
 
 % ========================================================================
 % Load the data
-data = load('training_data_full_cov.mat');
+data = load('training_data_full_cov_case2.mat');
 xtrain = data.x_tr;
 ytrain = data.y_tr;
 xvalid = data.x_val;
@@ -23,7 +23,7 @@ error_covs = data.error_covs_test;
 % Set NN attributes
 net.task           = 'regression';
 net.saveModel      = 0;
-net.maxEpoch       = 20;
+net.maxEpoch       = 30;
 % GPU
 net.gpu            = false;
 net.cuda           = false;
@@ -55,150 +55,193 @@ net.actFunIdx      = [0         4       0     ];
 
 % Gain factors
 mw_gain = 1;
-Sw_gain = 0.75;
+% Sw_gain = 0.75;
 mb_gain = 1;
-Sb_gain = 0.75;
+% Sb_gain = 0.75;
 % Parameter initialization
 net.initParamType  = 'He';
 numLayers = length(net.layer);
-net.gainMw = mw_gain.*ones(1, numLayers-1);
-net.gainSw = Sw_gain.*ones(1, numLayers-1);
-net.gainMb = mb_gain*ones(1, numLayers-1);
-net.gainSb = Sb_gain*ones(1, numLayers-1);
+% net.gainMw = mw_gain.*ones(1, numLayers-1);
+% net.gainSw = Sw_gain.*ones(1, numLayers-1);
+% net.gainMb = mb_gain*ones(1, numLayers-1);
+% net.gainSb = Sb_gain*ones(1, numLayers-1);
 
-attemps = 0;
-% while attemps < 20
-
-start = true;
-% seed = randi(1e8);
-seed = 42;
-shuffle = true;
-
-if ~isempty(seed)
-    rng(seed);
-    net.seed = seed;
-    fprintf('Seed: %d\n', seed);
-else
-    rng('shuffle');
-    rng_ = rng;
-    fprintf('Rng: %d\n', rng_.Seed);
-end
-
-% ========================================================================
-% Train the  model
-
-% Values used to find the optimal number of epochs for training
-% if ~start
-%     best_LL = -inf;
-% end
+attempts = 0;
+total_attempts = 1;
 best_LL = -inf;
-patience = 0;
-patience_threshold = 3;
 
-% Train net
-net.trainMode = 1;
-[net, states, maxIdx, netInfo] = network.initialization(net);
-normStat = tagi.createInitNormStat(net);
+gain_values = [0.01, 0.1, 0.5, 0.75, 1];
+hidden_nodes = [50, 100, 200, 400];
+gain_max = 1;
+gain_min = 1e-2;
+h_nodes_max = 500;
+h_nodes_min = 50;
 
-% Validation net
-netT              = net;
-netT.trainMode    = 0;
-[netT, statesT, maxIdxT] = network.initialization(netT); 
-normStatT = tagi.createInitNormStat(netT);
-
-theta = tagi.initializeWeightBias(net);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Training
-stop  = 0;
-epoch = 0;
-best_epoch = 0;
-tic
-
-while ~stop
-    fprintf('\nEpoch: %d \n==============\n', epoch)
-    
-    if epoch >1
-        idxtrain = randperm(size(ytrain, 1));
-        ytrain   = ytrain(idxtrain, :);
-        xtrain   = xtrain(idxtrain, :);
-    end
-
-    [theta, normStat, mytrain, Sytrain,...
-        ~] = network.regression(net, theta, normStat, states,...
-        maxIdx, xtrain, ytrain);
-    
-    if any(isnan(mytrain))
-        fprintf('\nWarn! NaN values in predicted mean\n');
-    end
-    if any(isnan(Sytrain))
-        fprintf('Warn! NaN values in oredicted variance\n');
-    end
-    
-%     LL_train = mt.loglik(ytrain, mytrain(:,1:net.nl), Sytrain(:,1:net.nl));
-%     if isnan(LL_train)
-%         LL_train = -inf;
-%     end
-
-%     fprintf('Train L.L. : %d \n',LL_train)
-%     fprintf('--------------\n')
-
-    % Validating
-    [~, ~, myvalid, Syvalid] = network.regression(netT, theta, ...
-    normStatT, statesT, maxIdxT, xvalid);
-
-    mv2_test = zeros(size(myvalid(:,net.nl+1:end)));
-    Sv2_test = zeros(size(Syvalid(:,net.nl+1:end)));
-    
-    for i=1:size(myvalid,1)
-        ma = myvalid(i,:);
-        Sa = Syvalid(i,:);
-        [~, mLa] = tagi.detachMeanVar(ma, net.nl,...
-            net.nLchol, net.batchSize, net.repBatchSize);
-        [~, SLa] = tagi.detachMeanVar(Sa, net.nl,...
-            net.nLchol, net.batchSize, net.repBatchSize);
-        % transform the diagonal elements into positive domain
-        [mLa_, SLa_, ~] = agvi.transform_chol_vec(mLa, SLa, net.gpu);
-        % retrieve the variance parameters from the cholesky vectors
-        [mv2, Sv2] = agvi.chol_to_v2(mLa_, SLa_);
-        mv2_test(i,:) = mv2;
-        Sv2_test(i,:) = diag(Sv2);
-    end
-
-    LL_val = mt.loglik(yvalid, myvalid(:,1:net.nl), Syvalid(:,1:net.nl) + mv2_test(:,[1,3,6])); %[1,3,6] are the indices of the variance parameters
-    if isnan(LL_val)
-        LL_val = -inf;
-    end
-    fprintf('Valid L.L. : %d \n',LL_val)
-
-    if LL_val > best_LL
-        best_LL = LL_val;
-        best_theta = theta;
-        best_epoch = epoch;
-        % best_seed = seed;
-        patience = 0;
+config = {};
+gridsearch = false;
+seed = [];
+while attempts < total_attempts
+    %     seed = randi(1e8);
+    %     seed = 99793097;
+    if ~isempty(seed)
+        rng(seed);
+        net.seed = seed;
+        fprintf('---------------------------------\n')
+        fprintf('Seed: %d\n', seed);
+        config.seed = seed;
     else
-        patience = patience + 1;
+        rng('shuffle');
+        rng_ = rng;
+        fprintf('---------------------------------\n')
+        fprintf('Rng: %d\n', rng_.Seed);
+        config.rng = rng_;
     end
-    
-    epoch = epoch + 1;
-    
-    if epoch > net.maxEpoch
-        fprintf('Epoch > %d, max epoch exceeded. Training terminated... best epoch: %d \n', net.maxEpoch, best_epoch)
-        stop = true;
-    elseif patience == patience_threshold
-        fprintf('Early stopping... Training terminated at epoch %d, best epoch: %d \n', epoch-1, best_epoch)
-        stop = true;
+
+    if gridsearch
+        mw_gain = 1;
+        mb_gain = 1;
+        Sw_gain = randsample(gain_values,1);
+        Sb_gain = randsample(gain_values,1);
+        h_nodes = randsample(hidden_nodes,1);
+    else % randomized search
+%         mw_gain = (gain_max-gain_min).*rand + gain_min;
+%         mb_gain = (gain_max-gain_min).*rand + gain_min;
+        mw_gain = 1;
+        mb_gain = 1;
+        Sw_gain = (gain_max-gain_min).*rand + gain_min;
+        Sb_gain = (gain_max-gain_min).*rand + gain_min;
+        h_nodes = randi([h_nodes_min, h_nodes_max]);
     end
+
+    net.gainMw = mw_gain.*ones(1, numLayers-1);
+    net.gainSw = Sw_gain.*ones(1, numLayers-1);
+    net.gainMb = mb_gain*ones(1, numLayers-1);
+    net.gainSb = Sb_gain*ones(1, numLayers-1);
+    net.nodes = [net.nx    h_nodes     net.ny]; 
+
+    config.mw_gain = mw_gain;
+    config.mb_gain = mb_gain;
+    config.Sb_gain = Sb_gain;
+    config.Sw_gain = Sw_gain;
+    config.layers = net.nodes;
+
+
+    % ========================================================================
+    % Train the  model
+    
+    % Values used to find the optimal number of epochs for training
+    % best_LL = -inf;
+    patience = 0;
+    patience_threshold = 10;
+    
+    % Train net
+    net.trainMode = 1;
+    [net, states, maxIdx, netInfo] = network.initialization(net);
+    normStat = tagi.createInitNormStat(net);
+    
+    % Validation net
+    netT              = net;
+    netT.trainMode    = 0;
+    [netT, statesT, maxIdxT] = network.initialization(netT); 
+    normStatT = tagi.createInitNormStat(netT);
+    
+    theta = tagi.initializeWeightBias(net);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Training
+    stop  = 0;
+    epoch = 0;
+    best_epoch = 0;
+%     tic
+%     config
+    while ~stop
+        fprintf('\nEpoch: %d \n==============\n', epoch)
+        
+        if epoch >1
+            idxtrain = randperm(size(ytrain, 1));
+            ytrain   = ytrain(idxtrain, :);
+            xtrain   = xtrain(idxtrain, :);
+        end
+    
+        [theta, normStat, mytrain, Sytrain,...
+            ~] = network.regression(net, theta, normStat, states,...
+            maxIdx, xtrain, ytrain);
+        
+        if any(isnan(mytrain))
+            fprintf('\nWarn! NaN values in predicted mean\n');
+        end
+        if any(isnan(Sytrain))
+            fprintf('Warn! NaN values in oredicted variance\n');
+        end
+        
+    %     LL_train = mt.loglik(ytrain, mytrain(:,1:net.nl), Sytrain(:,1:net.nl));
+    %     if isnan(LL_train)
+    %         LL_train = -inf;
+    %     end
+    
+    %     fprintf('Train L.L. : %d \n',LL_train)
+    %     fprintf('--------------\n')
+    
+        % Validating
+        [~, ~, myvalid, Syvalid] = network.regression(netT, theta, ...
+        normStatT, statesT, maxIdxT, xvalid);
+    
+        mv2_test = zeros(size(myvalid(:,net.nl+1:end)));
+        Sv2_test = zeros(size(Syvalid(:,net.nl+1:end)));
+        
+        for i=1:size(myvalid,1)
+            ma = myvalid(i,:);
+            Sa = Syvalid(i,:);
+            [~, mLa] = tagi.detachMeanVar(ma, net.nl,...
+                net.nLchol, net.batchSize, net.repBatchSize);
+            [~, SLa] = tagi.detachMeanVar(Sa, net.nl,...
+                net.nLchol, net.batchSize, net.repBatchSize);
+            % transform the diagonal elements into positive domain
+            [mLa_, SLa_, ~] = agvi.transform_chol_vec(mLa, SLa, net.gpu);
+            % retrieve the variance parameters from the cholesky vectors
+            [mv2, Sv2] = agvi.chol_to_v2(mLa_, SLa_);
+            mv2_test(i,:) = mv2;
+            Sv2_test(i,:) = diag(Sv2);
+        end
+    
+        LL_val = mt.loglik(yvalid, myvalid(:,1:net.nl), Syvalid(:,1:net.nl) + mv2_test(:,[1,3,6])); %[1,3,6] are the indices of the variance parameters
+        if isnan(LL_val)
+            LL_val = -inf;
+        end
+        fprintf('Valid L.L. : %d \n',LL_val)
+    
+        if LL_val > best_LL
+            best_LL = LL_val;
+            best_theta = theta;
+            best_epoch = epoch;
+            patience = 0;
+            best_config = config;
+            best_config.LL = best_LL;
+        else
+            patience = patience + 1;
+        end
+        
+        epoch = epoch + 1;
+        
+        if epoch > net.maxEpoch
+            fprintf('Epoch > %d, max epoch exceeded. Training terminated... best epoch: %d \n', net.maxEpoch, best_epoch)
+            stop = true;
+        elseif patience == patience_threshold
+            fprintf('Early stopping... Training terminated at epoch %d, best epoch: %d \n', epoch-1, best_epoch)
+            stop = true;
+        end
+    end
+    attempts = attempts + 1;
 end
-% attemps = attemps + 1;
-% end
-toc
+% toc
 fprintf('************************************\n')
-% fprintf('Best seed: %d', best_seed)
+% fprintf('\nBest seed: %d', best_seed)
+fprintf('\nBest LL_val: %d', best_LL)
+% best_config
 % ========================================================================
 % Predict
-[pred_mean, pred_var] = TAGI_predict(net, theta, x_test);
+[~, ~, pred_mean, pred_var] = network.regression(netT, best_theta, ...
+    normStatT, statesT, maxIdxT, x_test, []);
 
 % Transform from the 'Cholesky space' to the 'variance space'
 mv2_test = zeros(size(pred_mean(:,net.nl+1:end)));
@@ -222,9 +265,8 @@ end
 % Plot the predicted target variables
 var_indices = [1, 3, 6];
 for i=1:net.nl
-    j = var_indices(i);
     figure;
-    pl.regression(x_test, pred_mean(:,i), pred_var(:,i) + mv2_test(j,:), 'black', 'green', 1)
+    pl.regression(x_test, pred_mean(:,i), pred_var(:,i) + mv2_test(:,var_indices(i)), 'black', 'green', 1)
     hold on
     scatter([xtrain;xvalid], [ytrain(:,i);yvalid(:,i)], 10, 'magenta', 'o', 'DisplayName','training data')
 %     hold on
@@ -242,14 +284,14 @@ for i=1:net.nLchol
     row_ind = triu_ind_i(i);
     col_ind = triu_ind_j(i);
     if row_ind == col_ind
-        label = strcat('$\sigma_{',num2str(row_ind),'}^2$');
+        label = strcat('$\sigma_{',num2str(row_ind),'}^2$ true');
     else
-        label = strcat('$\sigma_{',strcat(num2str(row_ind),num2str(col_ind)),'}$');
+        label = strcat('$\sigma_{',strcat(num2str(row_ind),num2str(col_ind)),'}$ true');
     end
     figure;
     pl.regression(x_test, mv2_test(:,i), Sv2_test(:,i), 'black', 'green', 1)
     hold on
     plot(x_test, error_covs(:,row_ind,col_ind),'DisplayName',label)
-    xlim([-0.75 0.75]) % limit the plot to the to the range of the training data
+%     xlim([-0.75 0.75]) % limit the plot to the to the range of the training data
 end
 
